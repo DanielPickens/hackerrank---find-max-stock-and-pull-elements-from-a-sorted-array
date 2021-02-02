d7bef8caf19eac0540268396335a3b8680d89afd
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-02 15:01:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def get_max_profit(prices):
-max_so_far = 0 i = 2 for price in prices:
-for next_price in prices[i::]: 
-profit = next_price - price
-if (profit > max_so_far):
-max_so_far = profit 
+   max_so_far = 0 
+    i = 2 
+    for price in prices:
+  for next_price in prices[i::]: 
+      profit = next_price - price
+  if (profit > max_so_far):
+    max_so_far = profit 
 i += 1 
-return max_so_far; 
+ return max_so_far; 
+
 print(get_max_profit([1, 2, 5, 3]))
 
 output: 4
